4dd216bece94d6952f442b55129e4a6b3531c6a5
Michael Lu <lu.michael@outlook.com>
2022-04-09 14:17:37 -0700

    update to work with python 3.7+

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'gym~=0.21.0',
         'joblib~=0.14.0',
-        'mujoco_py==2.0.2.7',
+        'mujoco_py<2.2,>= 2.1',
         'numpy~=1.19.5',
         'xmltodict~=0.12.0',
     ],
