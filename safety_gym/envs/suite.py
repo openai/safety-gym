5129f9070b3bd23c02968bda4fd0c840e5719fe7
davideferrari95 <davideferr@unimore.it>
2022-11-22 21:07:52 +0100

    Renamed Custom "mujoco.py" File
    mujoco.py -> safety_mujoco.py
    Avoid conflicts with the new library mujoco (import mujoco)
    Changes Entry Point
    Some cleaning of unused libraries in "suite.py"

--- safety_gym/envs/suite.py
+++ safety_gym/envs/suite.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-import numpy as np
 from copy import deepcopy
-from string import capwords
 from gym.envs.registration import register
-import numpy as np
-
 
 VERSION = 'v0'
 
@@ -70,7 +66,7 @@ class SafexpEnvBase:
             reg_config.update(robot_config)
             reg_config.update(config)
             register(id=env_name,
-                     entry_point='safety_gym.envs.mujoco:Engine',
+                     entry_point='safety_gym.envs.safety_mujoco:Engine',
                      kwargs={'config': reg_config})
             if MAKE_VISION_ENVIRONMENTS:
                 # Vision: note, these environments are experimental! Correct behavior not guaranteed
