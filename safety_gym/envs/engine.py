da84cbd193e042e10bb122da8c417e6fc99f3fb2
davideferrari95 <davideferr@unimore.it>
2023-03-01 20:57:14 +0100

    Fixed Manual Seeding in `reset()` Function

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -882,11 +882,11 @@ class Engine(gym.Env, gym.utils.EzPickle):
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         ''' Reset the physics simulation and return observation '''
-        self._seed += 1  # Increment seed
-        if seed is not None:
-            self._np_random, self.rs = gym.utils.seeding.np_random(seed)
-        else: 
-            self.rs = np.random.RandomState(self._seed)
+
+        # Manual or Random Seeding
+        self._seed = seed if seed is not None else np.random.randint(0,2**32)
+        self.rs = np.random.RandomState(self._seed)
+
         self.done = False
         self.steps = 0  # Count of steps taken in this episode
         # Set the button timer to zero (so button is immediately visible)
